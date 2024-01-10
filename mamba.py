f658b18d82699a8ac97f915203e980f4c8832364
Niklas Schmelzle <niklas.schmelzle@gmail.com>
2024-01-10 20:35:46 +0000

    Fixed tokenizer and added validation for tokenizer and dataset

--- mamba.py
+++ mamba.py
@@ -60,7 +60,7 @@ class CharacterTokenizer(PreTrainedTokenizer):
                 an id (starting at 7) will be assigned to each character.
             model_max_length (int): Model maximum sequence length.
         """
-        self.characters = characters
+        #self.characters = characters
         self.model_max_length = model_max_length
         bos_token = AddedToken("[BOS]", lstrip=False, rstrip=False)
         eos_token = AddedToken("[SEP]", lstrip=False, rstrip=False)
@@ -71,21 +71,13 @@ class CharacterTokenizer(PreTrainedTokenizer):
 
         mask_token = AddedToken("[MASK]", lstrip=True, rstrip=False)
 
-        self._vocab_str_to_int = {
-            "[CLS]": 0,
-            "[SEP]": 1,
-            "[BOS]": 2,
-            "[MASK]": 3,
-            "[PAD]": 4,
-            "[RESERVED]": 5,
-            "[UNK]": 6,
-            **{ch: i + 7 for i, ch in enumerate(characters)},
-        }
-        self._vocab_int_to_str = {v: k for k, v in self._vocab_str_to_int.items()}
+        #self._vocab_str_to_int = {
+            #**{ch: i + 0 for i, ch in enumerate(characters)},
+        #}
+        #self._vocab_int_to_str = {v: k for k, v in self._vocab_str_to_int.items()}
 
         super().__init__(
             bos_token=bos_token,
-            eos_token=sep_token,
             sep_token=sep_token,
             cls_token=cls_token,
             pad_token=pad_token,
@@ -97,98 +89,106 @@ class CharacterTokenizer(PreTrainedTokenizer):
             **kwargs,
         )
 
+        #for c in characters:
+            #token = AddedToken(c, lstrip=False, rstrip=False)
+            #self.add_tokens(token)
+        self.add_tokens(characters)
 
     @property
     def vocab_size(self) -> int:
-        return len(self._vocab_str_to_int)
+        return len(self.added_tokens_encoder)
 
-    def _tokenize(self, text: str) -> List[str]:
-        return list(text)
+    def get_vocab(self):
+        return self.added_tokens_encoder
 
-    def _convert_token_to_id(self, token: str) -> int:
-        return self._vocab_str_to_int.get(token, self._vocab_str_to_int["[UNK]"])
+#    def _tokenize(self, text: str) -> List[str]:
+#        print("_tokenize")
+#        return list(text)
 
-    def _convert_id_to_token(self, index: int) -> str:
-        return self._vocab_int_to_str[index]
+#    def _convert_token_to_id(self, token: str) -> int:
+#        print("_convert_token_to_id")
+#        return self.encode(token)
+
+#    def _convert_id_to_token(self, index: int) -> str:
+#        print("_convert_id_to_token")
+#        return self.decode(index)
 
     def convert_tokens_to_string(self, tokens):
         return "".join(tokens)
 
-    def convert_token_vector_to_string(self, ivector):
-        out_str = ""
-        for i in ivector:
-            out_str = out_str + self._convert_id_to_token(i.item())
-        return out_str
+#    def convert_token_vector_to_string(self, ivector):
+#        out_str = ""
+#        for i in ivector:
+#            out_str = out_str + self._convert_id_to_token(i.item())
+#        return out_str
 
-    def build_inputs_with_special_tokens(
-        self, token_ids_0: List[int], token_ids_1: Optional[List[int]] = None
-    ) -> List[int]:
-        sep = [self.sep_token_id]
-        cls = [self.cls_token_id]
-        result = cls + token_ids_0 + sep
-        if token_ids_1 is not None:
-            result += token_ids_1 + sep
-        return result
+#    def build_inputs_with_special_tokens(
+#        self, token_ids_0: List[int], token_ids_1: Optional[List[int]] = None
+#    ) -> List[int]:
+#        print("build_inpts_with_special_tokens")
+#        sep = [self.sep_token_id]
+#        cls = [self.cls_token_id]
+#        result = cls + token_ids_0 + sep
+#        if token_ids_1 is not None:
+#            result += token_ids_1 + sep
+#        return result
 
-    def get_vocab(self):
-        vocab = {chr(i): i for i in range(self.vocab_size)}
-        vocab.update(self.added_tokens_encoder)
-        return vocab
+#    def get_special_tokens_mask(
+#        self,
+#        token_ids_0: List[int],
+#        token_ids_1: Optional[List[int]] = None,
+#        already_has_special_tokens: bool = False,
+#    ) -> List[int]:
+#        print("get_special_tokens_mask")
+#        if already_has_special_tokens:
+#            return super().get_special_tokens_mask(
+#                token_ids_0=token_ids_0,
+#                token_ids_1=token_ids_1,
+#                already_has_special_tokens=True,
+#            )
 
-    def get_special_tokens_mask(
-        self,
-        token_ids_0: List[int],
-        token_ids_1: Optional[List[int]] = None,
-        already_has_special_tokens: bool = False,
-    ) -> List[int]:
-        if already_has_special_tokens:
-            return super().get_special_tokens_mask(
-                token_ids_0=token_ids_0,
-                token_ids_1=token_ids_1,
-                already_has_special_tokens=True,
-            )
+#        result = [1] + ([0] * len(token_ids_0)) + [1]
+#        if token_ids_1 is not None:
+#            result += ([0] * len(token_ids_1)) + [1]
+#        return result
 
-        result = [1] + ([0] * len(token_ids_0)) + [1]
-        if token_ids_1 is not None:
-            result += ([0] * len(token_ids_1)) + [1]
-        return result
+#    def create_token_type_ids_from_sequences(
+#        self, token_ids_0: List[int], token_ids_1: Optional[List[int]] = None
+#    ) -> List[int]:
+#        print("create_tokens_type_ids_from_sequence")
+#        sep = [self.sep_token_id]
+#        cls = [self.cls_token_id]
 
-    def create_token_type_ids_from_sequences(
-        self, token_ids_0: List[int], token_ids_1: Optional[List[int]] = None
-    ) -> List[int]:
-        sep = [self.sep_token_id]
-        cls = [self.cls_token_id]
+#        result = len(cls + token_ids_0 + sep) * [0]
+#        if token_ids_1 is not None:
+#            result += len(token_ids_1 + sep) * [1]
+#        return result
 
-        result = len(cls + token_ids_0 + sep) * [0]
-        if token_ids_1 is not None:
-            result += len(token_ids_1 + sep) * [1]
-        return result
+#    def get_config(self) -> Dict:
+#        return {
+#            "char_ords": [ord(ch) for ch in self.characters],
+#            "model_max_length": self.model_max_length,
+#        }
 
-    def get_config(self) -> Dict:
-        return {
-            "char_ords": [ord(ch) for ch in self.characters],
-            "model_max_length": self.model_max_length,
-        }
+#    @classmethod
+#    def from_config(cls, config: Dict) -> "CharacterTokenizer":
+#        cfg = {}
+#        cfg["characters"] = [chr(i) for i in config["char_ords"]]
+#        cfg["model_max_length"] = config["model_max_length"]
+#        return cls(**cfg)
 
-    @classmethod
-    def from_config(cls, config: Dict) -> "CharacterTokenizer":
-        cfg = {}
-        cfg["characters"] = [chr(i) for i in config["char_ords"]]
-        cfg["model_max_length"] = config["model_max_length"]
-        return cls(**cfg)
+#    def save_pretrained(self, save_directory: Union[str, os.PathLike], **kwargs):
+#        cfg_file = Path(save_directory) / "tokenizer_config.json"
+#        cfg = self.get_config()
+#        with open(cfg_file, "w") as f:
+#            json.dump(cfg, f, indent=4)
 
-    def save_pretrained(self, save_directory: Union[str, os.PathLike], **kwargs):
-        cfg_file = Path(save_directory) / "tokenizer_config.json"
-        cfg = self.get_config()
-        with open(cfg_file, "w") as f:
-            json.dump(cfg, f, indent=4)
-
-    @classmethod
-    def from_pretrained(cls, save_directory: Union[str, os.PathLike], **kwargs):
-        cfg_file = Path(save_directory) / "tokenizer_config.json"
-        with open(cfg_file) as f:
-            cfg = json.load(f)
-        return cls.from_config(cfg)
+#    @classmethod
+#    def from_pretrained(cls, save_directory: Union[str, os.PathLike], **kwargs):
+#        cfg_file = Path(save_directory) / "tokenizer_config.json"
+#        with open(cfg_file) as f:
+#            cfg = json.load(f)
+#        return cls.from_config(cfg)
 
 
 def create_genome_tokenizer(seq_len):
@@ -199,6 +199,30 @@ def create_genome_tokenizer(seq_len):
         padding_side='left',
     )
     return tokenizer
+
+def validate_tokenizer():
+    tokenizer = create_genome_tokenizer(1024)
+
+    assert tokenizer.vocab_size == 11, "unexpected vocab_size"
+
+    assert tokenizer.pad_token_id == 3, "unexpected id for pad token"
+    assert tokenizer.mask_token_id == 5, "unexpected id for mask token"
+
+    tokenizer_encode_dict = tokenizer.added_tokens_encoder
+    assert len(tokenizer_encode_dict) == 11, "unexpected tokenizer_encode_dict"
+    assert tokenizer_encode_dict["[BOS]"] == 0, "unexpected id for \"[BOS]\" token"
+    assert tokenizer_encode_dict["[UNK]"] == 1, "unexpected id for \"[UNK]\" token"
+    assert tokenizer_encode_dict["[SEP]"] == 2, "unexpected id for \"[SEP]\" token"
+    assert tokenizer_encode_dict["[PAD]"] == 3, "unexpected id for \"[PAD]\" token"
+    assert tokenizer_encode_dict["[CLS]"] == 4, "unexpected id for \"[CLS]\" token"
+    assert tokenizer_encode_dict["[MASK]"] == 5, "unexpected id for \"[MASK]\" token"
+    assert tokenizer_encode_dict["A"] == 6, "unexpected id for \"A\" token"
+    assert tokenizer_encode_dict["C"] == 7, "unexpected id for \"C\" token"
+    assert tokenizer_encode_dict["G"] == 8, "unexpected id for \"G\" token"
+    assert tokenizer_encode_dict["T"] == 9, "unexpected id for \"T\" token"
+    assert tokenizer_encode_dict["N"] == 10, "unexpected id for \"N\" token"
+
+    print("Successfull validation of DNATokenizer!")
 
 
 def complement(in_seq):
@@ -321,8 +345,54 @@ class GenomeIterator:
 
         # mask
         target = inpt.clone()
-        inpt[-1] = self.tokenizer._vocab_str_to_int['[MASK]']
+        inpt[-1] = self.tokenizer.mask_token_id
         return inpt, target
+
+
+    # validate T2T dataset; check if correct tokens are returned for predefined indices
+    def validate_T2T_ds():
+        train_iter = GenomeIterator(GenomeIterator.T2T_path, GenomeIterator.training_entries, 42)
+
+        token_len = 30
+        tokenizer = create_genome_tokenizer(token_len)
+
+        train_iter.config(tokenizer, token_len)
+        print(train_iter.entry_ranges)
+
+        def check(idx, expct_inpt, expct_trgt):
+            inpt, trgt = train_iter.get_seq(idx)
+            actual_trgt = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(trgt))
+            actual_inpt = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(inpt))
+
+            # Does not recognize "PAD" and "MASK" token
+            # expct_inpt_len = len(tokenizer(expct_inpt, add_special_tokens=False, max_length=token_len,
+            #                            truncation=False)["input_ids"])
+            # assert expct_inpt_len == token_len, \
+            #     "Unexpected token length of expct_inpt ({})".format(expct_inpt_len)
+
+            assert expct_trgt == actual_trgt, \
+                                "Target tokens do not match; expected: {} (len {}), actual: {} (len {})".format(expct_trgt, len(expct_trgt), actual_trgt, len(actual_trgt))
+            assert expct_inpt == actual_inpt, \
+                                "Input tokens do not match; expected: {} (len {}), actual: {} (len {})".format(expct_inpt, len(expct_inpt), actual_inpt, len(actual_inpt))
+            print("tokens match!")
+
+        # forward
+        check(5, "[PAD]"*24 + "CACCC" + "[MASK]", "[PAD]"*24 + "CACCC" + "T")
+        check(248387322, "GGGTTAGGGTTAGGGTTAGGGTTAGGGTT" + "[MASK]", "GGGTTAGGGTTAGGGTTAGGGTTAGGGTT" + "A")
+        check(1067810436, "[PAD]"*5 + "CCTAACCCTAACCCTAACCCCTAA" + "[MASK]", "[PAD]"*5 + "CCTAACCCTAACCCTAACCCCTAA" + "C")
+        check(1228377838, "GGGTTAGGGTTAGGGGTTAGGGTTAGGGT" + "[MASK]", "GGGTTAGGGTTAGGGGTTAGGGTTAGGGT" + "T")
+        check(2786358510, "CTAACCCTAACCCTAACCCTAACCCTAAC" + "[MASK]", "CTAACCCTAACCCTAACCCTAACCCTAAC" + "C")
+        check(2848818478, "AGGGTTAGGGTTAGGGTTAGGGTTAGGGT" + "[MASK]", "AGGGTTAGGGTTAGGGTTAGGGTTAGGGT" + "T")
+        # reverse complement
+        offset = 2848818499
+        check((5 + offset), "GTTAGGGTTAGGGTTAGGGGTTAGGGTTT" + "[MASK]", "GTTAGGGTTAGGGTTAGGGGTTAGGGTTT" + "A")
+        check(248387322 + offset, "[PAD]"*24 + "AACCC" + "[MASK]", "[PAD]"*24 + "AACCC" + "T")
+        check(1067810436 + offset, "GTTAGGAGGGTTAGGGGATTAGGGTTAGG" + "[MASK]", "GTTAGGAGGGTTAGGGGATTAGGGTTAGG" + "G")
+        check(1228377838 + offset, "[PAD]"*28 + "T" + "[MASK]", "[PAD]"*28 + "T" + "A")
+        check(2786358510 + offset, "GTTAGGGTTAGGGTTAGGGTTAGGGTTAG" + "[MASK]", "GTTAGGGTTAGGGTTAGGGTTAGGGTTAG" + "G")
+        check(2848818478 + offset, "[PAD]"*9 + "CTAACCCTAACCCTAACCCT" + "[MASK]", "[PAD]"*9 + "CTAACCCTAACCCTAACCCT" + "A")
+
+        print("Succesfull validation of T2T dataset access!")
 
 
 class GenomeDataset(torch.utils.data.IterableDataset):
@@ -477,8 +547,10 @@ def mamba_training():
 def main(args):
     if args.download_data:
         GenomeDataset.download_t2t_data()
-    if args.validate_dataloader:
-        raise NotImplementedError("validate dataloader")
+    if args.validate_dataset:
+        GenomeIterator.validate_T2T_ds()
+    if args.validate_tokenizer:
+        validate_tokenizer()
     if args.run_training:
         mamba_training();
 
@@ -486,7 +558,8 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog="MambaDNA")
     parser.add_argument("--download_data", action="store_true", help="download the genetic data")
-    parser.add_argument("--validate_dataloader", action="store_true", help="validate dataloader/tokenizer")
+    parser.add_argument("--validate_dataset", action="store_true", help="validate access of T2T dataset")
+    parser.add_argument("--validate_tokenizer", action="store_true", help="validate tokenizer")
     parser.add_argument("-r", "--run_training", action="store_true", help="run training")
     args = parser.parse_args()
 
