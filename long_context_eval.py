5ba0d1c2131e2f00e00c662c05526dc302a3f898
Niklas Schmelzle <niklas.schmelzle@gmail.com>
2024-05-06 22:49:22 +0000

    text generation with hf-mamba model and own mamba model

--- long_context_eval.py
+++ long_context_eval.py
@@ -1,23 +1,61 @@
-from transformers import MambaConfig, MambaForCausalLM, AutoTokenizer
+#from transformers import MambaConfig, MambaForCausalLM, AutoTokenizer, AutoConfig
+from transformers import AutoTokenizer, AutoModelForCausalLM
 import datasets
 import torch
+
+from mamba_ssm.models.mixer_seq_simple import MambaLMHeadModel
+from mamba_ssm.models.config_mamba import MambaConfig
 
 
 def main():
     tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neox-20b")
     #tokenizer = AutoTokenizer.from_pretrained("state-spaces/mamba-2.8b-hf")
-    model = MambaForCausalLM.from_pretrained("state-spaces/mamba-2.8b-hf").to("cuda")
+
+    #mamba_config = AutoConfig.from_pretrained("state-spaces/mamba-2.8b-hf")
+    hf_model = AutoModelForCausalLM.from_pretrained("state-spaces/mamba-2.8b-hf") #.to("cuda")
+    #print(type(hf_model))
+    #print(hf_model.config)
+    hf_state_dict = hf_model.state_dict()
+    hf_state_dict['backbone.embedding.weight'] = hf_state_dict.pop('backbone.embeddings.weight')
+
+    mamba_config = MambaConfig(n_layer=64, d_model=2560, vocab_size=50280,
+                               ssm_cfg={}, rms_norm=True, residual_in_fp32=True, fused_add_norm=True,
+                               pad_vocab_size_multiple=1)
+    model = MambaLMHeadModel(mamba_config)
+
+    # Compare keys in both models
+    hf_keys = set(hf_state_dict.keys())
+    model_keys = set(model.state_dict().keys())
+
+    # Find missing and unexpected keys
+    missing_keys = model_keys - hf_keys
+    unexpected_keys = hf_keys - model_keys
+
+    print("Missing keys:", missing_keys)
+    print("Unexpected keys:", unexpected_keys)
+
+    model.load_state_dict(hf_state_dict)
+    model = model.to("cuda")
+    
+    print(sum(p.numel() for p in model.parameters()))
 
     input_texts = datasets.load_dataset("PY007/tokenized_proof_pile_test_neox", split="test")
     input_texts = input_texts.filter(lambda x: x["tokenized_len"] >= 32768, num_proc=64)
     input_texts = input_texts[:20]
 
-    #print(input_texts)
-    #print(tokenizer.bos_token)
+    tokens = [x for x in range(2048, 122889, 2048)]
+    #print(tokens)
 
     input_ids = tokenizer("Explain the difference between a FPGA and a CPU?", return_tensors="pt")["input_ids"].to("cuda")
-    out = model.generate(input_ids, max_new_tokens=500).to("cpu")
+
+    out = model.generate(input_ids, max_length=500).to("cpu")
+    print("mamba")
     for a in tokenizer.batch_decode(out):
+        print(a)
+
+    hf_out = hf_model.to("cuda").generate(input_ids, max_new_tokens=500).to("cpu")
+    print("hf_mamba")
+    for a in tokenizer.batch_decode(out_hf):
         print(a)
 
 
