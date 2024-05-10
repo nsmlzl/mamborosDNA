e23956a809e2ca48d4ca35c3337dcdd8c347e071
Niklas Schmelzle <niklas.schmelzle@gmail.com>
2024-05-10 21:14:58 +0000

    compute perplexity for range of context-length and visualize results

--- long_context_eval.py
+++ long_context_eval.py
@@ -1,3 +1,9 @@
+import argparse
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from pathlib import Path
+
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import datasets
 import torch
@@ -6,10 +12,10 @@ from mamba_ssm.models.mixer_seq_simple import MambaLMHeadModel
 from mamba_ssm.models.config_mamba import MambaConfig
 
 
-def main():
+def compute(args):
     tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neox-20b")
 
-    hf_model = AutoModelForCausalLM.from_pretrained("state-spaces/mamba-2.8b-hf") #.to("cuda")
+    hf_model = AutoModelForCausalLM.from_pretrained("state-spaces/mamba-2.8b-hf")
     hf_state_dict = hf_model.state_dict()
     hf_state_dict['backbone.embedding.weight'] = hf_state_dict.pop('backbone.embeddings.weight')
 
@@ -19,25 +25,74 @@ def main():
     model = MambaLMHeadModel(mamba_config)
     model.load_state_dict(hf_state_dict)
     model = model.to("cuda")
+    model.eval()
     
+
     input_texts = datasets.load_dataset("PY007/tokenized_proof_pile_test_neox", split="test")
     input_texts = input_texts.filter(lambda x: x["tokenized_len"] >= 32768, num_proc=64)
-    input_texts = input_texts[:20]
+    input_texts = input_texts[:50]
 
-    tokens = [x for x in range(2048, 122889, 2048)]
+    df = None
 
-    input_ids = tokenizer("Explain the difference between a FPGA and a CPU?", return_tensors="pt")["input_ids"].to("cuda")
+    for context_length in range(1024, 15*1024+1, 1024):
+        torch.cuda.empty_cache()
+        ppls = []
+        encoded_texts = input_texts["input_ids"]
 
-    out = model.generate(input_ids, max_length=500).to("cpu")
-    print("mamba")
-    for a in tokenizer.batch_decode(out):
-        print(a)
+        encoded_texts = [x[0:context_length] for x in encoded_texts]
 
-    hf_out = hf_model.to("cuda").generate(input_ids, max_new_tokens=500).to("cpu")
-    print("hf_mamba")
-    for a in tokenizer.batch_decode(hf_out):
-        print(a)
+        for idx in range(0, len(encoded_texts)):
+            input_ids = torch.tensor([encoded_texts[idx]]).to("cuda")
+            target_ids = input_ids[:, 1:].clone().contiguous()
+
+            with torch.no_grad():
+                logits = model(input_ids).logits[:, :-1, :].contiguous()
+                cross_entropy = torch.nn.functional.cross_entropy(logits.view(-1, logits.size(-1)), target_ids.view(-1), reduction='mean')
+                ppl = torch.exp(cross_entropy)
+                ppls.append(ppl.item())
+
+        df_new = pd.DataFrame([{'context_length': context_length, 'ppls': ppls}])
+        df = df_new if df is None else pd.concat([df, df_new], ignore_index=True)
+        print(f"context length {context_length} done!")
+
+    df.to_pickle(args.file)
+
+
+def visualize(args):
+    fig = plt.figure()
+    for file in args.files:
+        df = pd.read_pickle(file)
+
+        vis_data = np.zeros((len(df['context_length']), 3))
+        for (idx, context_length) in enumerate(df['context_length']):
+            ppls = np.array(df[df['context_length']==context_length]['ppls'].item())
+
+            vis_data[idx, 0] = context_length
+            vis_data[idx, 1] = ppls.mean()
+            vis_data[idx, 2] = ppls.std()
+
+        label = Path(file).stem
+        plt.errorbar(vis_data[:, 0], vis_data[:, 1], yerr=vis_data[:, 2], marker='x', capsize=6, label=label)
+
+    plt.xlabel("Context Length")
+    plt.ylabel("Perplexity")
+    plt.legend()
+    plt.savefig(args.fig_file, dpi=300, bbox_inches='tight')
+    print("plot created")
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(prog="long_context_eval")
+    subparsers = parser.add_subparsers(dest="subcommand", required=True)
+
+    comp_sp = subparsers.add_parser("compute", help="compute perplexity for multiple context lengths")
+    comp_sp.add_argument("--file", type=str, default="data.pkl", help="output file path", metavar="path/to/data.pkl")
+    comp_sp.set_defaults(func=compute)
+
+    vis_sp = subparsers.add_parser("visualize", help="visualize perplexity")
+    vis_sp.add_argument("--files", type=str, default=["data.pkl"], nargs='+', help="input file path", metavar="path/to/data.pkl")
+    vis_sp.add_argument("--fig_file", type=str, default="fig.png", help="output figure path", metavar="path/to/fig.png")
+    vis_sp.set_defaults(func=visualize)
+
+    args = parser.parse_args()
+    args.func(args)
